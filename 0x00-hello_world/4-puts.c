f0e1968248ca67110923527cffae5bd69dded9ca
Marwanfouz <marwanfouz76@gmail.com>
2023-02-16 11:04:52 -0800

    4 task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return : Akways 0 (Success)
+ */
 int main (void){
-  puts("\"Programming is like building a multilingual puzzle")
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
