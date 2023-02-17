962c1722aa525b1a0f859ae4b47b761291b644bd
Marwanfouz <92979905+Marwanfouz@users.noreply.github.com>
2023-02-17 16:01:44 +0200

    2 task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char i;
-	for (i = 'a'; i <= 'z'; i++)
-		putchar(i);
-		putchar("\n");
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
+
 	return (0);
 }
