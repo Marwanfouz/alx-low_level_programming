9505890627740f8437cea4289526ab76b0c7bfdb
Marwanfouz <92979905+Marwanfouz@users.noreply.github.com>
2023-02-17 16:56:28 +0200

    9 task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar('\t');
+	}
+	putchar('\n');
+	return (0);
+}
