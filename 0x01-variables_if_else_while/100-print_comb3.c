4fc46f9886361f17f4990978a4e4703ac485a8c0
Marwanfouz <92979905+Marwanfouz@users.noreply.github.com>
2023-02-17 17:28:57 +0200

    10 task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 0; i <= 8; i++)
+	{
+			for (j = 0; j <= 9; j++)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+	if (i < 9 && j < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
