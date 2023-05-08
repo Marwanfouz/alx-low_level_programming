663ba928a292fde185aa99e3fbd75462a1a27173
Marwan Fouz <92979905+Marwanfouz@users.noreply.github.com>
2023-05-08 18:33:49 +0300

    task 0

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
